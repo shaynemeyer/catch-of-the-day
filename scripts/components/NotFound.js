9d63a46361a1004e848070fab92956d9a8449256
Shayne <shayne@Shaynes-MacBook-Pro-3.local>
2016-02-14 10:04:14 -0800

    updated NotFound to ES6

--- scripts/components/NotFound.js
+++ scripts/components/NotFound.js
@@ -3,12 +3,12 @@
 */
 import React from 'react';
 
-var NotFound = React.createClass({
-  render: function(){
+class NotFound extends React.Component {
+  render(){
     return (
       <h1>Not Found!</h1>
-    );
+    )
   }
-});
+}
 
 export default NotFound;
